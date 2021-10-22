47bade2af503e079a9a901a0a7b6802e8028ee1e
AndEditor7 <andeditor7@gmail.com>
2021-10-22 11:18:56 +1100

    Add animation for totems

--- src/main/java/net/linkle/valley/Registry/WeaponsAndTools/Totems/AbstractTotemBase.java
+++ src/main/java/net/linkle/valley/Registry/WeaponsAndTools/Totems/AbstractTotemBase.java
@@ -1,14 +1,17 @@
 package net.linkle.valley.Registry.WeaponsAndTools.Totems;
 
 import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -28,6 +31,16 @@ public abstract class AbstractTotemBase extends Item {
         PlayerEntity playerEntity = user instanceof PlayerEntity ? (PlayerEntity)user : null;
         if (playerEntity instanceof ServerPlayerEntity serverEntity) {
             Criteria.CONSUME_ITEM.trigger(serverEntity, stack);
+        }
+        
+        if (world.isClient) {
+            var client = MinecraftClient.getInstance();
+            client.particleManager.addEmitter(user, ParticleTypes.TOTEM_OF_UNDYING, 30);
+            if (user == client.player) {
+                client.gameRenderer.showFloatingItem(stack);
+            }
+        } else {
+            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ITEM_TOTEM_USE, user.getSoundCategory(), 1, 1);
         }
 
         if (playerEntity != null) {
