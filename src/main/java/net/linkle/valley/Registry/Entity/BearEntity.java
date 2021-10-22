5b8ff6232206586b75309f424738f5fe8ebf0ff3
AndEditor7 <andeditor7@gmail.com>
2021-10-22 18:10:35 +1100

    Add brown bear

---
+++ src/main/java/net/linkle/valley/Registry/Entity/BearEntity.java
@@ -0,0 +1,20 @@
+package net.linkle.valley.Registry.Entity;
+
+import net.linkle.valley.Registry.Initializers.Entities;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.PolarBearEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+
+/** Extended from PolarBearEntity class. You can override methods and make changes. */
+public class BearEntity extends PolarBearEntity {
+	public BearEntity(EntityType<? extends PolarBearEntity> entityType, World world) {
+		super(entityType, world);
+	}
+	
+	@Override
+	public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return Entities.BEAR.create(world);
+    }
+}
