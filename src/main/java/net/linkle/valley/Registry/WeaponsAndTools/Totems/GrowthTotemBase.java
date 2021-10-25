c19a0739755ffbf25c8084869766760ec9d8610f
AndEditor7 <andeditor7@gmail.com>
2021-10-25 18:54:51 +1100

    Add new textures for mossy carmine block
    The textures is made by SaltySeaDoggo.

--- src/main/java/net/linkle/valley/Registry/WeaponsAndTools/Totems/GrowthTotemBase.java
+++ src/main/java/net/linkle/valley/Registry/WeaponsAndTools/Totems/GrowthTotemBase.java
@@ -1,16 +1,14 @@
 package net.linkle.valley.Registry.WeaponsAndTools.Totems;
 
+import java.util.List;
+
 import net.minecraft.advancement.criterion.Criteria;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -18,8 +16,6 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
-
-import java.util.List;
 
 import static net.linkle.valley.Registry.Initializers.MiscItems.G_TOTEM;
 
