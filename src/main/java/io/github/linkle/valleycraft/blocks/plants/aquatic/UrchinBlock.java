8ae4d2a75ed60b352f7887f0a2d6a3f097a1e92a
AndEditor7 <andeditor7@gmail.com>
2022-01-12 17:44:48 +1100

    Update UrchinBlock class

--- src/main/java/io/github/linkle/valleycraft/blocks/plants/aquatic/UrchinBlock.java
+++ src/main/java/io/github/linkle/valleycraft/blocks/plants/aquatic/UrchinBlock.java
@@ -1,16 +1,13 @@
 package io.github.linkle.valleycraft.blocks.plants.aquatic;
 
-import io.github.linkle.valleycraft.init.Entities;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.GuardianEntity;
+import net.minecraft.entity.mob.WaterCreatureEntity;
+import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-
-import static io.github.linkle.valleycraft.init.Entities.FIRE_EEL;
 
 public class UrchinBlock extends AquaticBlock {
     
@@ -20,15 +17,12 @@ public class UrchinBlock extends AquaticBlock {
 
     @Override
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        entity.slowMovement(state, new Vec3d(0.9, 0.85, 0.9));
-        if (entity instanceof LivingEntity && entity.getType() != EntityType.TROPICAL_FISH && entity.getType() != EntityType.SALMON
-                && entity.getType() != EntityType.PUFFERFISH  && entity.getType() != EntityType.COD  && entity.getType() != EntityType.SQUID
-                && entity.getType() != EntityType.GLOW_SQUID  && entity.getType() != EntityType.GUARDIAN  && entity.getType() != EntityType.ELDER_GUARDIAN
-                && entity.getType() != EntityType.AXOLOTL && entity.getType() != Entities.FIRE_EEL) {
-            if (state.get(WATERLOGGED)) {
-                entity.damage(DamageSource.CACTUS, 2.0F);
-            }
-            entity.damage(DamageSource.CACTUS, 2.0F);
+        if (entity instanceof WaterCreatureEntity && 
+            entity instanceof GuardianEntity && 
+            entity instanceof AxolotlEntity) {
+            return;
         }
+        
+        entity.damage(DamageSource.CACTUS, 2.0F);
     }
 }
