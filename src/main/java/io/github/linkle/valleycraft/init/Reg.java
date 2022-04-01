ddd09dbaec68e7b9aa5e783326244c4dcb875b3a
Owner <Owner@192.168.0.2>
2022-04-01 14:03:28 +1100

    Fixed broken data and assets resources

--- src/main/java/io/github/linkle/valleycraft/init/Reg.java
+++ src/main/java/io/github/linkle/valleycraft/init/Reg.java
@@ -17,49 +17,54 @@ import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.placementmodifier.PlacementModifier;
 
 public class Reg {
-	public static Item register(String id, Item item) {
-	    return Registry.register(Registry.ITEM, newId(id), item);
-	}
+    public static Item register(String id, Item item) {
+        return Registry.register(Registry.ITEM, newId(id), item);
+    }
 
-	public static Block register(String id, Block block) {
-	    return Registry.register(Registry.BLOCK, newId(id), block);
-	}
+    public static Block register(String id, Block block) {
+        return Registry.register(Registry.BLOCK, newId(id), block);
+    }
 
-	public static void register(String id, BlockItem item) {
-	    var identifier = newId(id);
-	    Registry.register(Registry.BLOCK, identifier, item.getBlock());
-	    Registry.register(Registry.ITEM, identifier, item);
-	}
+    public static void register(String id, BlockItem item) {
+        var identifier = newId(id);
+        Registry.register(Registry.BLOCK, identifier, item.getBlock());
+        Registry.register(Registry.ITEM, identifier, item);
+    }
 
-	public static void registerWithItem(String id, Block block, Item.Settings settings) {
-	    var identifier = newId(id);
-	    block = Registry.register(Registry.BLOCK, identifier, block);
-	    Registry.register(Registry.ITEM, identifier, new BlockItem(block, settings));
-	}
-    
+    public static void registerWithItem(String id, Block block, Item.Settings settings) {
+        var identifier = newId(id);
+        block = Registry.register(Registry.BLOCK, identifier, block);
+        Registry.register(Registry.ITEM, identifier, new BlockItem(block, settings));
+    }
+
     public static <C extends FeatureConfig> Feature<C> register(String id, Feature<C> feature) {
         return Registry.register(Registry.FEATURE, newId(id), feature);
     }
-    
+
     public static <T> RegistryEntry<T> add(Registry<T> registry, String id, T value) {
         return BuiltinRegistries.add(registry, newId(id), value);
     }
-    
-    public static RegistryEntry<PlacedFeature> register(String id, RegistryEntry<? extends ConfiguredFeature<?, ?>> entry, List<PlacementModifier> modifiers) {
+
+    public static RegistryEntry<PlacedFeature> register(String id,
+            RegistryEntry<? extends ConfiguredFeature<?, ?>> entry, List<PlacementModifier> modifiers) {
         return add(BuiltinRegistries.PLACED_FEATURE, id, new PlacedFeature(RegistryEntry.upcast(entry), modifiers));
     }
-    
-    public static RegistryEntry<PlacedFeature> register(String id, ConfiguredFeature<?, ?> config, List<PlacementModifier> modifiers) {
+
+    public static RegistryEntry<PlacedFeature> register(String id, ConfiguredFeature<?, ?> config,
+            List<PlacementModifier> modifiers) {
         var identifier = newId(id);
         var entry = BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_FEATURE, identifier, config);
-        return BuiltinRegistries.add(BuiltinRegistries.PLACED_FEATURE, identifier, new PlacedFeature(RegistryEntry.upcast(entry), modifiers));
+        return BuiltinRegistries.add(BuiltinRegistries.PLACED_FEATURE, identifier,
+                new PlacedFeature(RegistryEntry.upcast(entry), modifiers));
     }
-    
-    public static RegistryEntry<PlacedFeature> register(String id, ConfiguredFeature<?, ?> config, PlacementModifier... modifiers) {
+
+    public static RegistryEntry<PlacedFeature> register(String id, ConfiguredFeature<?, ?> config,
+            PlacementModifier... modifiers) {
         return register(id, config, List.of(modifiers));
     }
-    
-    public static RegistryEntry<PlacedFeature> register(String id, RegistryEntry<? extends ConfiguredFeature<?, ?>> entry, PlacementModifier... modifiers) {
+
+    public static RegistryEntry<PlacedFeature> register(String id,
+            RegistryEntry<? extends ConfiguredFeature<?, ?>> entry, PlacementModifier... modifiers) {
         return register(id, entry, List.of(modifiers));
     }
 }
